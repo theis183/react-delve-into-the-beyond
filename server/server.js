4a5d3d19fc2578a44b097b16d098b732508d36ca
Ian Theis <iandbtheis@gmail.com>
2019-06-27 20:15:49 -0500

    config

--- server/server.js
+++ server/server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose")
+const path = require("path")
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client/build"));
 
 
 // Define API routes here
